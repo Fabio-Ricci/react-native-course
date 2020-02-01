6164fca5fe196a82011a31de0e8d4b48b6726537
Fabio-Ricci <fabioricci9@gmail.com>
2020-02-01 20:49:40 -0300

    blog

---
+++ blog/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { Provider } from './src/context/BlogContext';
+import IndexScreen from './src/screens/IndexSceen';
+
+const navigator = createStackNavigator({
+  Index: IndexScreen
+}, {
+  initialRouteName: 'Index',
+  defaultNavigationOptions: {
+    title: 'Blogs'
+  }
+});
+
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <Provider>
+      <App />
+    </Provider>
+  );
+};
